9403245b6e5aa222deabfd8866e78efbeb8d78cb
Pineappologist <anastasia.romashka@gmail.com>
2021-11-20 19:53:23 +0100

    Initial commit

---
+++ app/src/main/java/com/caffeinesoftware/ultraschool/DayScreen.java
@@ -0,0 +1,10 @@
+package com.caffeinesoftware.ultraschool;
+
+import androidx.fragment.app.Fragment;
+
+public class DayScreen extends Fragment {
+    public DayScreen() {
+        super(R.layout.day_screen);
+    }
+
+}
